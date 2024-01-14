8df6fc071eb000a37eaa6ef0a583dfbd18b08fdf
Stephàn Costa <stephan.acosta1@outlook.com>
2024-01-14 16:32:15 -0300

    models

---
+++ src/models/tweet.model.ts
@@ -0,0 +1,15 @@
+import { randomUUID } from "crypto";
+import { Usuario } from "./usuario.model";
+
+export class Tweet {
+    
+    id: string;
+    conteudo: string;
+    tipoTweet: string;
+
+    constructor(public usuarioId: Usuario, conteudo: string, tipoTweet: string) {
+        this.id = randomUUID();
+        this.conteudo = conteudo;
+        this.tipoTweet = tipoTweet;
+    }
+}
