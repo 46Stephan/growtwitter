29e23a6e3463d5d507fe109aadfad257c392b658
Stephàn Costa <stephan.acosta1@outlook.com>
2024-01-14 20:54:42 -0300

    rotas

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,44 @@
-import { Express } from "express";
-import { CorsOptions } from "cors";
+import  express  from "express";
+import cors from "cors";
+import { UsuarioController } from "./controllers/user.controller";
+import { TweetController } from "./controllers/tweet.controller";
+import { AuthController } from "./controllers/auth.controller";
+
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const usuarioController = new UsuarioController();
+const tweetController = new TweetController();
+const authController = new AuthController();
+
+
+// PARA USUÁRIO
+
+app.post("/usuario", usuarioController.criarUsuario);
+app.get("/usuario/:id", usuarioController.buscarUsuario);
+app.get("/usuario/", usuarioController.listarUsuarios);
+app.put("/usuario/:id", usuarioController.editarUsuario);
+app.delete("/usuario/:id", usuarioController.deletarUsuario);
+
+
+// PARA TWEET
+
+app.post("/usuario/:id/tweet", tweetController.criarTweet);
+app.get("/tweets", tweetController.listarTweet);
+app.put("/tweet/:id", tweetController.editarTweets);
+app.get("/tweet/:id", tweetController.buscarTweetId);
+app.delete("/tweet/:id", tweetController.deletarTweets);
+
+// PARA AUTENTICAÇÃO
+
+app.post("/login", authController.login);
+
+
+
+
+
+app.listen(3000, () => {
+    console.log("Servidor rodando na porta 3000 - http://localhost:3000");
+});
